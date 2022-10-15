60ad34a68ade9d3f04c2f10583ac7a449a6d6103
Kecseti <zsolt.kecseti@gmail.com>
2022-10-15 14:08:12 +0200

    Tulajdonos.class, BankiSzolgaltatas hozzaadva

---
+++ src/com/petrik/bankiszolgaltatasok/BankiSzolgaltatas.java
@@ -0,0 +1,14 @@
+package com.petrik.bankiszolgaltatasok;
+
+public abstract class BankiSzolgaltatas {
+
+    private Tulajdonos tulajdonos;
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+}
